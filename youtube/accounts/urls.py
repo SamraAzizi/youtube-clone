c2c466ecb6d3948572e4c2515dd1213805868588
SamraAzizi <samarraazizi@gmail.com>
2026-01-19 17:50:25 +0500

    django youtube clone

---
+++ youtube/accounts/urls.py
@@ -0,0 +1,11 @@
+from django.url import path
+from django.contrib.auth import views as auth_views
+from . import views
+
+app_name = 'accounts'
+
+urlpatterns = [
+    path('register/', views.RegisterView.as_view(), name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html', redirected_authenticated_user=True), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='accounts/logout.html'), name='logout'),
+]
