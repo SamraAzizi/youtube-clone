1ea495d657502811db9b765c5c431a05d9a5bf11
SamraAzizi <samarraazizi@gmail.com>
2026-01-21 20:05:46 +0500

    django youtube clone

--- youtube/accounts/urls.py
+++ youtube/accounts/urls.py
@@ -1,4 +1,4 @@
-from django.url import path
+from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
 
@@ -6,6 +6,6 @@ app_name = 'accounts'
 
 urlpatterns = [
     path('register/', views.RegisterView.as_view(), name='register'),
-    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html', redirected_authenticated_user=True), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html', redirect_authenticated_user=True), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='accounts/logout.html'), name='logout'),
 ]
--- youtube/accounts/views.py
+++ youtube/accounts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirected
+from django.shortcuts import redirect, render 
 from django.contrib.auth import login
 from django.views.generic import CreateView
 from .forms import CustomUserCreationForm
@@ -12,10 +12,10 @@ class RegisterView(CreateView):
 
     def dispatch(self, request, *args, **kwargs):
         if request.user.is_authenticated:
-            return redirected('/')
+            return redirect('/')
         return super().dispatch(request, *args, **kwargs)
     
     def form_valid(self, form):
         response = super().form_valid(form)
         login(self.request, self.object)
-        return redirected("/")
+        return redirect("/")
