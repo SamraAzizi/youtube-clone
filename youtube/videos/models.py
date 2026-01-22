4e25f66158be63851ce3f19ba40617906e87cdfe
SamraAzizi <samarraazizi@gmail.com>
2026-01-22 19:41:57 +0500

    django youtube clone

--- youtube/videos/models.py
+++ youtube/videos/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Video(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='videos')
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+
+
+    file_id = models.CharField(max_length=255, unique=True)
+    video_url = models.URLField(max_length=500)
+    thumbnail_url = models.URLField(max_length=500, blank=True)
+
+    view = models.PositiveIntegerField(default=0)
+    likes = models.PositiveIntegerField(default=0)
+    dislikes = models.PositiveIntegerField(default=0)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.title
