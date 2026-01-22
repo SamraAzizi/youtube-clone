8f37a9d0d5fcf6aeb31576bf064d66a758df12bd
SamraAzizi <samarraazizi@gmail.com>
2026-01-22 20:07:36 +0500

    django youtube clone

---
+++ youtube/videos/forms.py
@@ -0,0 +1,43 @@
+from django import forms
+
+
+class VideoForm(forms.Form):
+    title = forms.CharField(
+        widget=forms.TextInput(
+        attrs={
+            "class":"form-input",
+            "placeholder":"Enter video title",
+        }
+        ) 
+    )
+    
+      
+    description = forms.CharField(
+        required=False
+        widget=forms.TextArea(
+        attrs={
+            "class":"form-input",
+            "placeholder":"Enter video description",
+            "row" : 4
+        }
+        )
+    )
+    video_file = forms.FileField(
+        widget=forms.FileInput(attrs={
+            "class":"form-input",
+            "accept":"video/*",
+        })
+
+    )
+
+    def clean_video_file(self):
+        video = self.cleaned_data.get("video_file")
+        if video:
+            if video.size > 100 * 1024 * 1024:  # 100 MB limit
+                raise forms.ValidationError("Video file size should not exceed 100 MB.")
+            
+            allowed_types = ["video/mp4", "video/webm", "video/x/msvideo"]
+            if video.content_type not in allowed_types:
+                raise forms.ValidationError("This Type of videos are not allowed.")
+        return video
+        
