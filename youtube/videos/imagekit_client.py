314abb00aabae608bca777941aaa456952969bce
SamraAzizi <samarraazizi@gmail.com>
2026-01-23 15:14:44 +0500

    youtube clone

--- youtube/videos/imagekit_client.py
+++ youtube/videos/imagekit_client.py
@@ -22,3 +22,28 @@ def upload_image(file_data:bytes, file_name:str, folder: str = "videos"):
         "file_id": response.file_id,
         "url": response.url,
     }
+
+def upload_thumbnail(file_data:bytes, file_name:str, folder: str = "thumbnails"):
+    import base64
+    public_key = os.environ.get("IMAGEKIT_PUBLIC_KEY")
+
+    if file_data.startswith("data:"):
+        base64_data = file_data.split(",")[1]
+        image_bytes = base64.b64decode(base64_data)
+    else:
+        image_bytes = base64.b64decode(file_data)
+
+    client = get_imagekit_client()
+
+    response = client.files.upload(
+        file = image_bytes
+        file_name= file_name,
+        folder= folder,
+        public_key= public_key
+    )  
+
+
+    return {
+        "file_id": response.file_id,
+        "url": response.url,
+    }
