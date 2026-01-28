852af07006cc256086e2203a77402b8d2e554514
SamraAzizi <samarraazizi@gmail.com>
2026-01-28 18:13:25 +0500

    youtube clone

--- youtube/videos/urls.py
+++ youtube/videos/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path("", views.video_list, name="list"),
     path('upload/', views.video_upload_page, name='upload'),
     path('upload/submit/', views.video_upload, name='upload_submit'),
+    path('<int:video_id>/', views.video_detail, name='detail'),
 
 ]
